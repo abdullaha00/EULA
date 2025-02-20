1a6675207e259cc278c6601ba06e5551140db214
asasieni <alexandersasieni@gmail.com>
2025-02-20 14:16:06 +0000

    changed port

--- text_extraction/run.sh
+++ text_extraction/run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
-USER="$(whoami)"
 SOCK_PATH="/home/as3495/EULA/text_extraction/web.sock"
 [ -e "$SOCK_PATH" ] && rm "$SOCK_PATH"
 
 umask 0
 
 . ~/.nvm/nvm.sh
-NODE_ENV=production PORT="$SOCK_PATH" \
+NODE_ENV=production PORT=3001 \
     exec node /home/as3495/EULA/text_extraction/index.js
