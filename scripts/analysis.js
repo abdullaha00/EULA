0dec44336e991d92875393270a97471d6798d9dc
tvrdikvoj <tvrdikvoj@gmail.com>
2025-03-04 09:44:18 +0000

    add support for saving custom analysis

--- scripts/analysis.js
+++ scripts/analysis.js
@@ -5,14 +5,46 @@ submitBtn = document.getElementById('submitPastedEulaBtn');
 if (submitBtn) {
     pastedEula = document.getElementById('pastedEula');
     if (pastedEula) {
-        submitBtn.addEventListener('click', () => {analyse(pastedEula.value);});
+        submitBtn.addEventListener('click', () => {handleAnalyseButton(pastedEula.value);});
     }
 }
 
 resultArea = document.getElementById('analysisResult');
 
-function analyse(text) {
+function handleAnalyseButton(text) {
     if (resultArea) {
+        // this should replace resultArea with a graph view of the analysis
+        // analysis = analyse(text);
+        // resultArea.innerHTML = visualise(analysis);
         resultArea.textContent = "EULA submitted: " + text;
+        
+        // add objects necessary to save analysis
+        var centerDiv = document.createElement('div');
+        centerDiv.classList.add('center-items');
+        resultArea.appendChild(centerDiv);
+
+        var nameInput = document.createElement('input');
+        nameInput.type = 'text';
+        nameInput.id = 'analysisName';
+        nameInput.placeholder = 'Service name';
+        centerDiv.appendChild(nameInput);
+
+        var saveBtn = document.createElement('button');
+        saveBtn.textContent = 'Save';
+        centerDiv.appendChild(saveBtn);
+        analysis = text; // delete once analysis is implemented
+        saveBtn.addEventListener('click', () => {saveAnalysis(analysis);});
     }
+}
+
+function saveAnalysis(analysis) {
+    var nameInput = document.getElementById('analysisName');
+    var key = nameInput.value;
+    chrome.storage.local.get([key], function(result) {
+        if (result[key] !== undefined) {
+            alert('An analysis with this name already exists.');
+        } else {
+            chrome.storage.local.set({ [key]: analysis });
+        }
+    });
 }
