cb23fb0cc287f6c40cfecbdcd9b4d15a3fb32414
kses171 <kses171@gmail.com>
2025-03-07 23:13:54 +0000

    minor change

--- text_extraction/llmparse.js
+++ text_extraction/llmparse.js
@@ -5,7 +5,7 @@ async function getLLMResponse(prompt) {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": "Bearer ef9b9a575e205f70254b76599c69aa5b4499d5815ed200f6e8fdec2f0af83d6b"
+                "Authorization": "Bearer d9a96b122fbb7c131ba05a0f3c67ffda5eba4f2db58f6e5c5edff31d887550c1"
             },
             body: JSON.stringify({
                 model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
